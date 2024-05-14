cfdfe39f50f5e7041b97bf5e5a459058c8ecdd6a
Jan Nowak <jano0121sc@gmail.com>
2024-05-14 08:05:40 +0200

    book component basic

---
+++ src/login-form/LoginForm.tsx
@@ -0,0 +1,78 @@
+import React, { useCallback, useMemo } from 'react';
+import './LoginForm.css';
+import { Button, TextField } from '@mui/material';
+import { Formik } from 'formik';
+import * as yup from 'yup';
+
+function LoginForm() {
+  const onSubmit = useCallback(
+    (values: { username: string; password: string }, formik: any) => {
+      console.log(values);
+    },
+    [],
+  );
+
+  const validationSchema = useMemo(
+    () =>
+      yup.object().shape({
+        username: yup.string().required('Username is required'),
+        password: yup
+          .string()
+          .required('Password is required')
+          .min(5, 'Password must be longer than 5 signs'),
+      }),
+    [],
+  );
+  return (
+    <>
+      <Formik
+        initialValues={{ username: '', password: '' }}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+        validateOnChange
+        validateOnBlur
+      >
+        {(formik: any) => (
+          <form
+            className="Login-form"
+            id="signForm"
+            noValidate
+            onSubmit={formik.handleSubmit}
+          >
+            <TextField
+              id="username"
+              name="username"
+              label="User name"
+              variant="standard"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.username && !!formik.errors.username}
+              helperText={formik.touched.username && formik.errors.username}
+            />
+            <TextField
+              id="password"
+              name="password"
+              label="Password"
+              variant="standard"
+              type="password"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.password && !!formik.errors.password}
+              helperText={formik.touched.password && formik.errors.password}
+            />
+            <Button
+              variant="contained"
+              type="submit"
+              id="signForm"
+              disabled={!(formik.isValid && formik.dirty)}
+            >
+              Sign in
+            </Button>
+          </form>
+        )}
+      </Formik>
+    </>
+  );
+}
+
+export default LoginForm;
